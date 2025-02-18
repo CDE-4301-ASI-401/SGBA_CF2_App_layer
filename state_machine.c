785334bee58ac0f2541e8c9656969bbcaa601d3b
dylankhoo <dylankhoo@gmail.com>
2025-02-18 18:07:09 +0700

    <Fix> Drone ID to decimal

--- state_machine.c
+++ state_machine.c
@@ -568,8 +568,8 @@ bool priority = true;
           // DEBUG_PRINT("heading = %.2f\n", (double)heading);
           //Drone 12,13,14 Left-WF; Drone 15,16,17 Right-WF; Drone 18,19 Left-WF
           // FOR DEMO 1 REPLACE 13 WITH 7, REPLACE 17 WITH 6
-          DEBUG_PRINT("IM DRONE %d\n", my_id);
-          if (my_id==12 || my_id==13 || my_id==14 || my_id==18 || my_id==19  ) {
+          DEBUG_PRINT("IM DRONE %d\n", my_id_dec);
+          if (my_id_dec==12 || my_id_dec==13 || my_id_dec==14 || my_id_dec==18 || my_id_dec==19  ) {
             init_SGBA_controller(drone_dist_from_wall_1, drone_speed, heading, -1); //LEFT-WF = -1
           } else {
             init_SGBA_controller(drone_dist_from_wall_2, drone_speed, heading, 1); //RIGHT-WF = 1
