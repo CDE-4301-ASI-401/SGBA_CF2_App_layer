21b6fe4480d7dacf4099804a8a431d228d526fcc
Matthew <e1129809@u.nus.edu>
2025-02-23 15:14:52 +0800

    Comment out previous code from Kbuild and use only pillar_avoid.c

--- pillar_avoid.c
+++ pillar_avoid.c
@@ -8,6 +8,8 @@
 #include "deck.h"
 #include "range.h"
 #include "pillar_avoid.h"
+#include <time.h>
+#include <unistd.h>
 
 #define TAKEOFF_HEIGHT 0.5f
 #define FORWARD_SPEED 0.2f
@@ -16,14 +18,10 @@
 #define CRITICAL_DISTANCE 0.1f // 10cm threshold for turning
 #define FORWARD_DURATION 5  // Move forward for 5 seconds
 
-// Ensure range sensor functions are correctly declared
-extern float rangeFront(void);
-extern float rangeLeft(void);
-extern float rangeRight(void);
+#define M2T(ms) (ms) // Directly use milliseconds
+#define vTaskDelay(ms) usleep((ms) * 1000) // Sleep for `ms` milliseconds
+#define xTaskGetTickCount() ((uint32_t)clock() * 1000 / CLOCKS_PER_SEC) // Get time in milliseconds
 
-static bool isObstacleAhead(void) {
-    return rangeFront() < SAFE_DISTANCE;
-}
 
 void fly_forward_then_avoid(void) {
     setpoint_t sp = {0};
@@ -42,7 +40,7 @@ void fly_forward_then_avoid(void) {
     
     // Start obstacle avoidance
     while (true) {
-        if (rangeFront() < CRITICAL_DISTANCE) {
+        if (rangeFront < CRITICAL_DISTANCE) {
             sp.velocity.x = 0.0f; // Stop
             sp.velocity.y = 0.0f; // Stop sideways movement
             
